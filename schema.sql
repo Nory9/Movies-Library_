86d60c6d770a9646d271a28de2a8f7d95a16440f
Nory9 <noormisk94@gmail.com>
2024-04-27 09:16:11 +0000

    solved lab14

--- schema.sql
+++ schema.sql
@@ -1,7 +1,8 @@
 CREATE TABLE  movie(
+    id int NOT NULL PRIMARY KEY,
     title VARCHAR(255),
     genre VARCHAR(255),
     overview VARCHAR(255),
-    release_date VARCHAR(255),
+    release_date DATE,
     poster VARCHAR(255)
 );
